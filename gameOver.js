c08d18ca7470b1a7e62dc158dc182c50a66495f5
Angellus Paixão <angelluspereira.nave@gmail.com>
2015-04-23 11:12:12 -0300

    Create gameOver.js

---
+++ gameOver.js
@@ -0,0 +1,26 @@
+var GameOver = function()
+{
+	var bg = new Image();
+	bg.src = 'Images/bgLose.png';
+
+	var btBack = new Image();
+	btBack.src = 'Images/bt_voltar.png';
+	this.btBack = new Button(360,500,69,94,btBack);
+	
+	this.update = function()
+	{
+			if(collisionManager.mouse_vs_button(this.btBack) && mouse_press)
+				{
+					console.log("JOOJ");
+					sceneManager.changeScene(0);			
+				}
+	}
+	
+	this.draw = function()
+	{
+		graphics.clear();
+		
+		graphics.drawImages(bg,0,0,800,600);
+		this.btBack.draw();
+	}
+}
