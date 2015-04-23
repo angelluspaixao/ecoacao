62087376d60696a92f7152a88d31d9e3db5296cb
Angellus Paixão <angelluspereira.nave@gmail.com>
2015-04-23 11:21:05 -0300

    Create keyboardManager.js

---
+++ keyboardManager.js
@@ -0,0 +1,47 @@
+var KeyboardManager = function()
+{
+	this.keydown = function(e)
+	{
+		switch (e.keyCode)
+		{
+			case 37:
+				if (sceneManager.currentScene == "game")
+				{
+					sceneManager.keyLeft = true;
+				}
+				break;		
+				
+			case 39:
+				if (sceneManager.currentScene == "game")
+				{
+					sceneManager.keyRight = true;
+				}
+				break;					
+		}
+	}
+	
+	this.keyup = function(e)
+	{
+		switch (e.keyCode)
+		{				
+			case 37:
+				if (sceneManager.currentScene == "game")
+				{
+					sceneManager.keyLeft = false;
+				}
+				break;					
+				
+			case 39:
+				if (sceneManager.currentScene == "game")
+				{
+					sceneManager.keyRight = false;
+				}
+				break;						
+		}		
+	}
+}
+
+var keyboard = new KeyboardManager();
+
+document.addEventListener('keydown', keyboard.keydown, true);	
+document.addEventListener('keyup', keyboard.keyup, true);
